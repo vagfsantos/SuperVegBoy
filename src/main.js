00192f4cb8eaefee177cd6f16de1acb0b9e4e338
Vagner Ferreira Santos <vagner.santos@fiap.com.br>
2018-03-01 20:12:50 -0300

    testing new libraries features

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { Game, Assets } from '../lib/index';
+import { Game, Assets, World, Scene, GameObject} from '../lib/index';
 
 let game = Game.create({
   name: 'game',
@@ -9,4 +9,15 @@ let game = Game.create({
 
 Assets.loadImage(
   'name:../assets/imgs.jpg')
-console.log(game)
+
+let world = new World('World 1')
+
+let scene = new Scene(game)
+
+let obj = new GameObject()
+
+scene.addGameObject(obj)
+
+world.addScene(scene)
+
+world.init()

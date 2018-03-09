bf4ce8b803f3c2de6ffd2946081c60e198dc3f66
Vagner Ferreira Santos <vagner.santos@fiap.com.br>
2018-03-09 19:13:20 -0300

    testing new features

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { Game, Assets, World, Scene, GameObject} from '../lib/index';
+import { Game, Assets, World, Scene, GameObject, Events} from '../lib/index';
 
 let game = Game.create({
   name: 'game',
@@ -17,24 +17,68 @@ let scene = new Scene(game)
 let obj = new GameObject()
 
 obj.attr = {
-  x: 0
+  x: 0,
+  y: 0,
+  width: 50,
+  height: 50
 }
 
 obj.onUpdate(function(){
-  this.attr = {
-    x: this.attr.x + 1
+
+  let velocity = 4;
+
+  if( Events.keyIsPressed('RIGHT_ARROW') ) {
+    
+    this.attr = {
+      x: this.attr.x + velocity
+    }
+  }
+  
+  if( Events.keyIsPressed('LEFT_ARROW') ) {
+    
+    this.attr = {
+      x: this.attr.x - velocity
+    }
+  }
+
+  if( Events.keyIsPressed('UP_ARROW') ) {
+    
+    this.attr = {
+      y: this.attr.y - velocity
+    }
+  }
+
+  if( Events.keyIsPressed('DOWN_ARROW') ) {
+    
+    this.attr = {
+      y: this.attr.y + velocity
+    }
   }
 })
 
+
 obj.onDraw(function(ctx){
   ctx.fillStyle = "#000"
-  ctx.fillRect(this.__attr.x,0,50,50)
+  ctx.fillRect(this.attr.x, this.attr.y, this.attr.width, this.attr.height)
 })
+
+
+obj.addColiderBox({
+  x: 10,
+  y: 10,
+  width: 30,
+  height: 30
+})
+
+obj.activeDebugMode()
 
 scene.addGameObject(obj)
 
 scene.addMap('map')
 
+scene.setCamera(obj)
+
 world.addScene(scene)
+
 
 world.init()
